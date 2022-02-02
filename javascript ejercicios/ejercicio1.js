ecf2140b720bae633e9eb7d821bc8d1cd885f8d8
MaineKoon88 <89954728+MaineKoon88@users.noreply.github.com>
2022-02-01 21:51:42 -0300

    Add files via upload

---
+++ javascript ejercicios/ejercicio1.js
@@ -0,0 +1,29 @@
+function calculardistancia(){
+
+  var distancia = document.getElementById('distance').value;
+  if (distancia > 0 && distancia  <= 1000)
+    alert('pie');
+
+    else if (distancia > 1000 && distancia <=10000)
+    alert('bicicleta');
+
+    else if (distancia > 10000 && distancia <= 30000)
+    alert('colectivo');
+
+    else if (distancia > 30000 && distancia <= 100000)
+    alert('auto');
+
+    else if (distancia > 100000)
+    alert('avion');{
+
+    } {
+
+    }{
+
+    }{
+
+    }
+
+
+
+}
