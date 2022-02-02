025a24651f58f4969a1428ea940bc94af8901ffb
MaineKoon88 <89954728+MaineKoon88@users.noreply.github.com>
2022-02-01 23:48:53 -0300

    Add files via upload

---
+++ javascript ejercicios 2/ejercicio2.js
@@ -0,0 +1,11 @@
+
+var numeros = [0 , 5 , 6 , 267 , 2112 , 357 , 24 , 33 , 9];
+
+var mayor = 0;
+
+for (var i = 0; i < numeros.length; i++) {
+if  (numeros[i] > mayor) {
+  mayor - numeros[i];
+  alert('numero')
+}
+}
